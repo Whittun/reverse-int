7def60b6a791d24089b0bfcb35c9050590563ac5
Whittun <whittun@gmail.com>
2023-07-06 15:33:04 +0300

    feat: add a solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let stringNumber = Math.abs(n).toString().split("");
+    let finalNumber = "";
+    
+    for (let i = stringNumber.length - 1; i >= 0; i--) {
+        finalNumber += stringNumber[i];
+    }
+    
+    return +finalNumber
 }
